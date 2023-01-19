730333f73e4c631c99879ba6a2b5583019fdcc88
CharlyesRodrigues <charlyessouza@gmail.com>
2023-01-19 19:54:14 -0300

    Atualizado

---
+++ Readme.md
@@ -0,0 +1,38 @@
+Aplicação de Clima com OpenWeather 
+
+Ferramentas Ulizadas
+
+* CSS => responsável pela estilização do projeto,
+* HTML => linguagem de marcação,
+* JAVASCRIPT => linguagem de programação utilizada.
+
+
+Extensões para Visual Studio Code
+
+* Material Icon Theme
+* Auto Rename Tag
+* Live Server
+
+<!--  Google Fonts  --> 
+Ubuntu => instalei essa com todas suas variações
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+
+
+<!--   Font Awesome--> => ícones ir em cdn js => https://cdnjs.com/libraries/font-awesome
+<link rel="stylesheet" 
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
+      integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
+      crossorigin="anonymous" 
+      referrerpolicy="no-referrer"
+      
+/>
+
+API'S
+
+ * Weather API  cadastro => https://openweathermap.org/api
+  ( API utilizada (Current weather data) => https://api.openweathermap.org/data/2.5/weather?)
+ * CountryFlagsAPI          https://www.countryflagsapi.com/
---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+
+// Variáveis e seleção de elementos
+
+//const apiKey ="396c4ad39fcc07c9dbfe4bce05476d22";  //trocar chave minha
+
+const apiKey ="ba605efc18f1572f61892fe426f18a1a";  //trocar chave
+const apiContryUrl = "https://www.countryflagsapi.com/png/";
+
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funções
+
+//Acessar a API
+
+const getWeatherData = async(city) => {
+
+const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+//Para obter a resposta com fetch
+
+const res = await fetch(apiWeatherUrl) ;
+
+const data = await res.json() ;  //  Transformando os dados em objetos de JavaScripti
+
+// console.log(data); testando os dados
+
+return data;
+}
+
+/* Exibe os dados da PI
+const showWeatherData = (city) =>{   
+    
+    getWeatherData(city);
+
+} */// Dados testados no momento do colsole.log acima.
+
+
+const showWeatherData = async (city) =>{   
+    
+    const data = await  getWeatherData(city);
+    getWeatherData(city);
+
+cityElement.innerText = data.name;
+tempElement.innerText = parseInt(data.main.temp);
+descElement.innerText = data.weather[0].description; 
+
+weatherIconElement.setAttribute(
+"src",
+`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+
+countryElement.setAttribute("src", apiContryUrl + data.sys.country);
+
+humidityElement.innerText = `${data.main.humidity}%`;
+windElement.innerText = `${data.wind.speed}Km/h`;
+
+weatherContainer.classList.remove("hide");
+};
+
+//Eventos
+
+searchBtn.addEventListener("click",(e) => {
+
+e.preventDefault();
+const city = cityInput.value;
+
+showWeatherData(city);
+
+});
